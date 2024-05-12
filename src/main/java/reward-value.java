cc8bff549204ba59dffa20adcb2a93cdeb6c76ed
roseclara03 <117148269+roseclara03@users.noreply.github.com>
2024-05-12 22:17:43 +0530

    Update reward-value.java

--- src/main/java/reward-value.java
+++ src/main/java/reward-value.java
@@ -1,24 +1,34 @@
-
 public class RewardValue {
     private double cashValue;
     private int milesValue;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        
+        // Convert cash value to miles.
         this.milesValue = (int) (cashValue / 0.0035);
     }
+
     public RewardValue(int milesValue) {
         this.milesValue = milesValue;
-        
-       
+        // Convert miles value to cash.
+        this.cashValue = milesValue * 0.0035;
     }
+
     public double getCashValue() {
         return cashValue;
     }
 
-    // Method to get miles value.
     public int getMilesValue() {
         return milesValue;
+    }
+
+    // Method to convert from cash to miles.
+    public static int convertCashToMiles(double cashValue) {
+        return (int) (cashValue / 0.0035);
+    }
+
+    // Method to convert from miles to cash.
+    public static double convertMilesToCash(int milesValue) {
+        return milesValue * 0.0035;
     }
 }
